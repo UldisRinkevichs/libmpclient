723dd6d3c9aa8d5b5953afc27707f2b3ceb34640
UldisRinkevichs <top100pve@gmail.com>
2018-08-08 03:27:24 +0700

    Initial release

---
+++ README.md
@@ -0,0 +1,2 @@
+# Project for Visual Studio 2015 to generate lib files to statically link with MpClient.dll
+
---
+++ Source/libmpclient/libmpclient/main.c
@@ -0,0 +1,771 @@
+#include <Windows.h>
+#include "MpClient.h"
+
+#pragma warning(disable: 4100 6101 6054)
+
+HRESULT WINAPI MpAddDynamicSignatureFile()
+{
+	return 0;
+}
+
+HRESULT WINAPI MpAllocMemory()
+{
+	return 0;
+}
+
+HRESULT WINAPI MpAmsiCloseSession()
+{
+	return 0;
+}
+
+HRESULT WINAPI MpAmsiScan()
+{
+	return 0;
+}
+
+HRESULT WINAPI MpCleanControl()
+{
+	return 0;
+}
+
+HRESULT WINAPI MpCleanOpen()
+{
+	return 0;
+}
+
+HRESULT WINAPI MpCleanPrecheckStart()
+{
+	return 0;
+}
+
+HRESULT WINAPI MpCleanStart()
+{
+	return 0;
+}
+
+HRESULT WINAPI MpClientUtilExportFunctions()
+{
+	return 0;
+}
+
+HRESULT WINAPI MpClose()
+{
+	return 0;
+}
+
+HRESULT WINAPI MpConfigClose()
+{
+	return 0;
+}
+
+HRESULT WINAPI MpConfigDelValue()
+{
+	return 0;
+}
+
+HRESULT WINAPI MpConfigGetValue()
+{
+	return 0;
+}
+
+HRESULT WINAPI MpConfigGetValueAlloc()
+{
+	return 0;
+}
+
+HRESULT WINAPI MpConfigInitialize()
+{
+	return 0;
+}
+
+HRESULT WINAPI MpConfigIteratorClose()
+{
+	return 0;
+}
+
+HRESULT WINAPI MpConfigIteratorEnum()
+{
+	return 0;
+}
+
+HRESULT WINAPI MpConfigIteratorOpen()
+{
+	return 0;
+}
+
+HRESULT WINAPI MpConfigOpen()
+{
+	return 0;
+}
+
+HRESULT WINAPI MpConfigRegisterForNotifications()
+{
+	return 0;
+}
+
+HRESULT WINAPI MpConfigSetValue()
+{
+	return 0;
+}
+
+HRESULT WINAPI MpConfigUninitialize()
+{
+	return 0;
+}
+
+HRESULT WINAPI MpConfigUnregisterNotifications()
+{
+	return 0;
+}
+
+HRESULT WINAPI MpConveySampleSubmissionResult()
+{
+	return 0;
+}
+
+HRESULT WINAPI MpConveyUserChoiceForSampleList()
+{
+	return 0;
+}
+
+HRESULT WINAPI MpCreateComInstance()
+{
+	return 0;
+}
+
+HRESULT WINAPI MpDbgAllocMemory()
+{
+	return 0;
+}
+
+HRESULT WINAPI MpDebugExportFunctions()
+{
+	return 0;
+}
+
+HRESULT WINAPI MpDetectionEnumerate()
+{
+	return 0;
+}
+
+HRESULT WINAPI MpDetectionQuery()
+{
+	return 0;
+}
+
+HRESULT WINAPI MpDynamicSignatureEnumerate()
+{
+	return 0;
+}
+
+HRESULT WINAPI MpDynamicSignatureOpen()
+{
+	return 0;
+}
+
+HRESULT WINAPI MpElevateCleanHandle()
+{
+	return 0;
+}
+
+HRESULT WINAPI MpElevationHandleAcquire()
+{
+	return 0;
+}
+
+HRESULT WINAPI MpElevationHandleActivate()
+{
+	return 0;
+}
+
+HRESULT WINAPI MpElevationHandleAttach()
+{
+	return 0;
+}
+
+HRESULT WINAPI MpElevationHandleOpen()
+{
+	return 0;
+}
+
+HRESULT WINAPI MpFastMemoryScan()
+{
+	return 0;
+}
+
+HRESULT WINAPI MpFastMemoryScanOpen()
+{
+	return 0;
+}
+
+HRESULT WINAPI MpFlushLowfiCache()
+{
+	return 0;
+}
+
+HRESULT WINAPI MpForcedReboot()
+{
+	return 0;
+}
+
+HRESULT WINAPI MpGenerateSignature()
+{
+	return 0;
+}
+
+HRESULT WINAPI MpGenerateSignatureEx()
+{
+	return 0;
+}
+
+HRESULT WINAPI MpGenerateThreatReport()
+{
+	return 0;
+}
+
+HRESULT WINAPI MpGetCallistoDetections()
+{
+	return 0;
+}
+
+HRESULT WINAPI MpGetEngineVersion()
+{
+	return 0;
+}
+
+HRESULT WINAPI MpGetHIPSRuleInfo()
+{
+	return 0;
+}
+
+HRESULT WINAPI MpGetMAPSConnectivityStatusInfo()
+{
+	return 0;
+}
+
+HRESULT WINAPI MpGetRunningMode()
+{
+	return 0;
+}
+
+HRESULT WINAPI MpGetSampleChunk()
+{
+	return 0;
+}
+
+HRESULT WINAPI MpGetSampleListRequiringConsent()
+{
+	return 0;
+}
+
+HRESULT WINAPI MpIsGivenRunningModeSupported()
+{
+	return 0;
+}
+
+HRESULT WINAPI MpIsRtpAutoEnable()
+{
+	return 0;
+}
+
+HRESULT WINAPI MpManagerDisable()
+{
+	return 0;
+}
+
+HRESULT WINAPI MpManagerEnable()
+{
+	return 0;
+}
+
+HRESULT WINAPI MpManagerStatusQuery()
+{
+	return 0;
+}
+
+HRESULT WINAPI MpManagerStatusQueryEx()
+{
+	return 0;
+}
+
+HRESULT WINAPI MpManagerXBGMDisable()
+{
+	return 0;
+}
+
+HRESULT WINAPI MpManagerXBGMEnable()
+{
+	return 0;
+}
+
+HRESULT WINAPI MpMemoryScanStart()
+{
+	return 0;
+}
+
+HRESULT WINAPI MpNotificationRegister()
+{
+	return 0;
+}
+
+HRESULT WINAPI MpOfflineScanInstall()
+{
+	return 0;
+}
+
+HRESULT WINAPI MpOfflineScanStatusQuery()
+{
+	return 0;
+}
+
+HRESULT WINAPI MpOpen()
+{
+	return 0;
+}
+
+HRESULT WINAPI MpProductGenuineCheck()
+{
+	return 0;
+}
+
+HRESULT WINAPI MpQuarantineRequest()
+{
+	return 0;
+}
+
+HRESULT WINAPI MpQueryDefaultFolderGuardList()
+{
+	return 0;
+}
+
+HRESULT WINAPI MpQueryEngineConfigDword()
+{
+	return 0;
+}
+
+HRESULT WINAPI MpQueryFileTrustByHandle()
+{
+	return 0;
+}
+
+HRESULT WINAPI MpRemapCallistoDetections()
+{
+	return 0;
+}
+
+HRESULT WINAPI MpRemoveDynamicSignatureFile()
+{
+	return 0;
+}
+
+HRESULT WINAPI MpRequestSnooze()
+{
+	return 0;
+}
+
+HRESULT WINAPI MpSampleQuery()
+{
+	return 0;
+}
+
+HRESULT WINAPI MpSampleSubmit()
+{
+	return 0;
+}
+
+HRESULT WINAPI MpScanResult()
+{
+	return 0;
+}
+
+HRESULT WINAPI MpScanStartEx()
+{
+	return 0;
+}
+
+HRESULT WINAPI MpSetUacElevationDefaultWindowHandle()
+{
+	return 0;
+}
+
+HRESULT WINAPI MpSmartLockerEnable()
+{
+	return 0;
+}
+
+HRESULT WINAPI MpTelemetryAddToAverageDWORD()
+{
+	return 0;
+}
+
+HRESULT WINAPI MpTelemetryAddToStreamDWORD()
+{
+	return 0;
+}
+
+HRESULT WINAPI MpTelemetryAddToStreamDWORD64()
+{
+	return 0;
+}
+
+HRESULT WINAPI MpTelemetryAddToStreamString()
+{
+	return 0;
+}
+
+HRESULT WINAPI MpTelemetryIncrementDWORD()
+{
+	return 0;
+}
+
+HRESULT WINAPI MpTelemetryInitialize()
+{
+	return 0;
+}
+
+HRESULT WINAPI MpTelemetryIsOptIn()
+{
+	return 0;
+}
+
+HRESULT WINAPI MpTelemetryLiteralAddToAverageDWORD()
+{
+	return 0;
+}
+
+HRESULT WINAPI MpTelemetryLiteralAddToStreamDWORD()
+{
+	return 0;
+}
+
+HRESULT WINAPI MpTelemetryLiteralAddToStreamDWORD64()
+{
+	return 0;
+}
+
+HRESULT WINAPI MpTelemetryLiteralAddToStreamString()
+{
+	return 0;
+}
+
+HRESULT WINAPI MpTelemetryLiteralIncrementDWORD()
+{
+	return 0;
+}
+
+HRESULT WINAPI MpTelemetryLiteralSetDWORD()
+{
+	return 0;
+}
+
+HRESULT WINAPI MpTelemetryLiteralSetDWORD64()
+{
+	return 0;
+}
+
+HRESULT WINAPI MpTelemetryLiteralSetIfMaxDWORD()
+{
+	return 0;
+}
+
+HRESULT WINAPI MpTelemetryLiteralSetIfMinDWORD()
+{
+	return 0;
+}
+
+HRESULT WINAPI MpTelemetryLiteralSetString()
+{
+	return 0;
+}
+
+HRESULT WINAPI MpTelemetrySetConsent()
+{
+	return 0;
+}
+
+HRESULT WINAPI MpTelemetrySetDWORD()
+{
+	return 0;
+}
+
+HRESULT WINAPI MpTelemetrySetDWORD64()
+{
+	return 0;
+}
+
+HRESULT WINAPI MpTelemetrySetIfMaxDWORD()
+{
+	return 0;
+}
+
+HRESULT WINAPI MpTelemetrySetIfMinDWORD()
+{
+	return 0;
+}
+
+HRESULT WINAPI MpTelemetrySetString()
+{
+	return 0;
+}
+
+HRESULT WINAPI MpTelemetryUninitialize()
+{
+	return 0;
+}
+
+HRESULT WINAPI MpTelemetryUpdateUserConsent()
+{
+	return 0;
+}
+
+HRESULT WINAPI MpTelemetryUpload()
+{
+	return 0;
+}
+
+HRESULT WINAPI MpThreatHistoryRequest()
+{
+	return 0;
+}
+
+HRESULT WINAPI MpThreatLocalizedInfoQuery()
+{
+	return 0;
+}
+
+HRESULT WINAPI MpThreatRollup()
+{
+	return 0;
+}
+
+HRESULT WINAPI MpTriggerErrorHeartbeatReport()
+{
+	return 0;
+}
+
+HRESULT WINAPI MpTriggerHeartbeatOnUninstall()
+{
+	return 0;
+}
+
+HRESULT WINAPI MpTriggerStatusRefreshNotification()
+{
+	return 0;
+}
+
+HRESULT WINAPI MpUpdateControl()
+{
+	return 0;
+}
+
+HRESULT WINAPI MpUpdateEngine()
+{
+	return 0;
+}
+
+HRESULT WINAPI MpUpdatePlatform()
+{
+	return 0;
+}
+
+HRESULT WINAPI MpUpdateStart()
+{
+	return 0;
+}
+
+HRESULT WINAPI MpUpdateStartEx()
+{
+	return 0;
+}
+
+HRESULT WINAPI MputAddToAverageDWORD64Rpc()
+{
+	return 0;
+}
+
+HRESULT WINAPI MputAddToAverageDWORDRpc()
+{
+	return 0;
+}
+
+HRESULT WINAPI MpUtilsExportFunctions()
+{
+	return 0;
+}
+
+HRESULT WINAPI MputIncrementDWORD64Rpc()
+{
+	return 0;
+}
+
+HRESULT WINAPI MputIncrementDWORDRpc()
+{
+	return 0;
+}
+
+HRESULT WINAPI MputSetBoolRpc()
+{
+	return 0;
+}
+
+HRESULT WINAPI MputSetDWORD64Rpc()
+{
+	return 0;
+}
+
+HRESULT WINAPI MputSetDWORDRpc()
+{
+	return 0;
+}
+
+HRESULT WINAPI MputSetIfMaxDWORD64Rpc()
+{
+	return 0;
+}
+
+HRESULT WINAPI MputSetIfMaxDWORDRpc()
+{
+	return 0;
+}
+
+HRESULT WINAPI MputSetIfMinDWORD64Rpc()
+{
+	return 0;
+}
+
+HRESULT WINAPI MputSetIfMinDWORDRpc()
+{
+	return 0;
+}
+
+HRESULT WINAPI MputSetStringRpc()
+{
+	return 0;
+}
+
+HRESULT WINAPI MpWDEnable()
+{
+	return 0;
+}
+
+HRESULT WINAPI MpXBGMEnable()
+{
+	return 0;
+}
+
+HRESULT WINAPI MpXBGMFreeEvent()
+{
+	return 0;
+}
+
+HRESULT WINAPI MpXBGMGetData()
+{
+	return 0;
+}
+
+HRESULT WINAPI MpXBGMPutData()
+{
+	return 0;
+}
+
+HRESULT WINAPI MpXBGMUpdateIV()
+{
+	return 0;
+}
+
+HRESULT WINAPI WDStatus(
+	_Out_ BOOL* pfEnabled)
+{
+	return 0;
+}
+
+HRESULT WINAPI WDEnable(
+	_In_ BOOL fEnable)
+{
+	return 0;
+}
+
+HRESULT WINAPI MpManagerOpen(
+	_In_  DWORD dwReserved,
+	_Out_ PMPHANDLE phMpHandle)
+{
+	return 0;
+}
+
+HRESULT WINAPI MpHandleClose(
+	_In_ MPHANDLE hMpHandle)
+{
+	return 0;
+}
+
+void WINAPI MpFreeMemory(
+	_In_ PVOID pMemory)
+{
+}
+
+HRESULT WINAPI MpManagerVersionQuery(
+	_In_  MPHANDLE hMpHandle,
+	_Out_ PMPVERSION_INFO pVersionInfo)
+{
+	return 0;
+}
+
+HRESULT WINAPI MpErrorMessageFormat(
+	_In_  MPHANDLE hMpHandle,
+	_In_  HRESULT  hrError,
+	_Out_ LPWSTR   *pwszErrorDesc)
+{
+	return 0;
+}
+
+HRESULT WINAPI MpThreatEnumerate(
+	_In_  MPHANDLE hThreatEnumHandle,
+	_Out_ PMPTHREAT_INFO *ppThreatInfo)
+{
+	return 0;
+}
+
+HRESULT WINAPI MpThreatOpen(
+	_In_  MPHANDLE        hScanHandle,
+	_In_  MPTHREAT_SOURCE ThreatSource,
+	_In_  MPTHREAT_TYPE   ThreatType,
+	_Out_ PMPHANDLE       phThreatEnumHandle)
+{
+	return 0;
+}
+
+HRESULT WINAPI MpThreatQuery(
+	_In_      MPHANDLE                 hMpHandle,
+	_In_      MPTHREAT_ID              ThreatID,
+	_Out_     PMPTHREAT_INFO           *ppThreatInfo,
+	_Out_opt_ PMPTHREAT_LOCALIZED_INFO *ppThreatLocalizedInfo)
+{
+	return 0;
+}
+
+HRESULT WINAPI MpScanStart(
+	_In_     MPHANDLE          hMpHandle,
+	_In_     MPSCAN_TYPE       ScanType,
+	_In_     DWORD             dwScanOptions,
+	_In_opt_ PMPSCAN_RESOURCES pScanResources,
+	_In_opt_ PMPCALLBACK_DATA  pCallbackInfo,
+	_Out_    PMPHANDLE         phScanHandle)
+{
+	return 0;
+}
+
+HRESULT WINAPI MpScanControl(
+	_In_ MPHANDLE  hScanHandle,
+	_In_ MPCONTROL ScanControl)
+{
+	return 0;
+}
+
+BOOL WINAPI DllMain(
+	_In_ HINSTANCE hinstDLL,
+	_In_ DWORD     fdwReason,
+	_In_ LPVOID    lpvReserved
+)
+{
+	return 1;
+}
